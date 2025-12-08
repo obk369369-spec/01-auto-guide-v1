f352aefa239ade4f6699714da686043f6aac0c17
obk369369-spec <obk369369@gmail.com>
2025-12-08 11:18:04 +0900

    Delete main.ts

--- main.ts
+++
@@ -1,7 +0,0 @@
-// main.ts (레포지토리 루트에 저장)
-import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
-
-Deno.serve((req) => serveDir(req, {
-  fsRoot: "./public",
-  urlRoot: "",
-}));
